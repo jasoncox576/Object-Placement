e686132d8a3abaa3aeda7bb29daf7af93066ebe4
jasoncox576 <jasongenecox@gmail.com>
2019-01-15 20:50:27 -0600

    Matrix priors model (preliminary)

---
+++ matrix_priors.py
@@ -0,0 +1,123 @@
+import csv
+import spacy
+import numpy as np
+
+print("Loading SPACY...")
+nlp = spacy.load('en_core_web_lg')
+print("Loading complete")
+
+rows_dict = {}
+PRIOR_MATRIX = []
+
+
+def strip(word):
+    if word[-2] == '_':
+        word = word[:-2]
+    
+    return word
+
+def fetch_sim(w1, w2, prior_matrix):
+    row = rows_dict.get(w1)
+    col = rows_dict.get(w2)
+    
+    if (row == None):
+        w1_token = nlp(w1)    
+        next_closest_word = ''
+        max_sim = 0.0
+        for word in rows_dict:
+            stripped_word = strip(word)
+            word_token = nlp(stripped_word)
+            sim = w1_token.similarity(word_token)
+            if sim >= max_sim:
+                max_sim = sim 
+                next_closest_word = word 
+        
+        print(w1, next_closest_word)
+        row = rows_dict.get(next_closest_word)
+
+    # TODO clean up later
+
+    if (col == None):
+        w2_token = nlp(w2)    
+        next_closest_word = ''
+        max_sim = 0.0
+        for word in rows_dict:
+            stripped_word = strip(word)
+            word_token = nlp(stripped_word)
+            sim = w2_token.similarity(word_token)
+            if sim >= max_sim:
+                max_sim = sim 
+                next_closest_word = word 
+
+        print(w2, next_closest_word)
+        col = rows_dict.get(next_closest_word)
+   
+    return prior_matrix[row][col]
+
+
+def fill_matrix(filename):
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile)
+            #number of rows of the sparse matrix
+            row_count = 0
+
+            # comparison of A to A is always 1, hence prior is identity
+            
+            #just a declaration
+
+            for row in reader:
+                if reader.line_num == 1:
+                    row_count = int(row[0])
+                    prior_matrix = np.identity(row_count)
+                    continue
+
+                if reader.line_num <= row_count+1:
+                    rows_dict[row[0]] = int(row[1]) 
+                
+                #symmetric matrix, so we fill both entries
+                else:
+                    obj_1 = row[0] 
+                    obj_2 = row[1][1:]
+                    prob = float(row[2])
+                    row = rows_dict.get(obj_1)
+                    col = rows_dict.get(obj_2)
+
+                    prior_matrix[row][col] = prob
+                    prior_matrix[col][row] = prob
+        
+
+        return (prior_matrix, rows_dict)
+
+
+def fill_matrix_word2vec(rows_dict):
+
+    """ Fills a matrix with similarities between
+        word embeddings, except treats similarities
+        as non-deterministic probablities
+    """
+    
+    #NOTE: Initializing to identity is redundant, because diagonal gets
+    # filled in anyway as 1
+    prior_matrix = np.identity(len(rows_dict))
+    for word1 in rows_dict:
+        for word2 in rows_dict:
+            row = rows_dict.get(word1)
+            col = rows_dict.get(word2)
+
+            token1 = nlp(strip(word1)) 
+            token2 = nlp(strip(word2))
+
+            prior_matrix[row][col] = token1.similarity(token2) 
+
+    
+
+
+
+PRIOR_MATRIX, rows_dict = fill_matrix('dummy_priors.csv')
+print(rows_dict)
+print(str(fetch_sim('gum', 'nut', PRIOR_MATRIX)))
+print(str(fetch_sim('oatmeal', 'coffee', PRIOR_MATRIX)))
+
+
+
+
--- word2vec_eval.py
+++ word2vec_eval.py
@@ -2,6 +2,8 @@ import csv
 import spacy
 from nltk.corpus import wordnet as wn
 import numpy as np
+import matrix_priors
+import random
 
 print("Loading SPACY..")
 nlp = spacy.load('en_core_web_lg')
@@ -13,34 +15,55 @@ print("Loading complete")
 total_row_counter = 0
 embedding_correct_counter = 0
 wordnet_correct_counter = 0
+matrix_correct_counter = 0
+
+
+def get_synset_and_strip(word):
+    
+    synset_num = 0
+
+    if word[-2] == '_':
+        synset_num = int(word[-1]) 
+        word = word[:-2]
+    
+    synset = wn.synsets(word.replace(" ", "_"))[synset_num]    
+    return (synset, word)
+
 
 
 filename = "dummy_results.csv"
-with open(filename, newline='') as csvfile:
+prior_matrix, rows_dict = matrix_priors.fill_matrix("dummy_priors.csv")
+
+
+
+with open(filename) as csvfile:
     #reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     reader = csv.reader(csvfile)
     for row in reader:
+        if reader.line_num == 1:
+            continue
+
         row_result = row[27:32]
         
-        for i in range(len(row_result)-1):
-            row_result[i] = row_result[i][:-6] 
+        #for i in range(len(row_result)-1):
+            #row_result[i] = row_result[i][:-6] 
 
         primary = row_result[0]
+        #print("PRIMARY: " + primary)
 
-        primary_token = nlp(primary)
-        
-        primary_syn = wn.synsets(primary)[0]
+        primary_syn, primary_token = get_synset_and_strip(primary) 
+        primary_token = nlp(primary_token)
 
         answer = row_result[4]
 
-        token1 = nlp(row_result[1])
-        synset1 = wn.synsets(row_result[1])[0]
+        synset1, token1 = get_synset_and_strip(row_result[1])
+        token1 = nlp(token1)
 
-        token2 = nlp(row_result[2])
-        synset2 = wn.synsets(row_result[2])[0]
+        synset2, token2 = get_synset_and_strip(row_result[2])
+        token2 = nlp(token2)
 
-        token3 = nlp(row_result[3])
-        synset3 = wn.synsets(row_result[3])[0]
+        synset3, token3 = get_synset_and_strip(row_result[3])
+        token3 = nlp(token3)
 
 
         if answer == "Top":
@@ -60,12 +83,45 @@ with open(filename, newline='') as csvfile:
         wordnet_sim_vector.append(primary_syn.path_similarity(synset2))
         wordnet_sim_vector.append(primary_syn.path_similarity(synset3))
 
-        
+       
+        matrix_prob_vector = [] 
+        matrix_prob_vector.append(prior_matrix[rows_dict.get(primary)][rows_dict.get(row_result[1])])
+        matrix_prob_vector.append(prior_matrix[rows_dict.get(primary)][rows_dict.get(row_result[2])])
+        matrix_prob_vector.append(prior_matrix[rows_dict.get(primary)][rows_dict.get(row_result[3])])
 
         if np.argmax(embedding_sim_vector) == answer:
             embedding_correct_counter += 1
         if np.argmax(wordnet_sim_vector) == answer:
             wordnet_correct_counter += 1
+        """
+        if np.argmax(matrix_prob_vector) == answer:
+            matrix_correct_counter += 1
+        """
+
+        # Computing the choice for the prob matrix isn't
+        # deterministic, so it is slightly more complicated.
+        # We must roll for probabilities.
+        
+        #This should be [0,1,2] by default but will generalize to n dimensions
+        possible_indices = [*range(len(matrix_prob_vector))]
+
+        while len(possible_indices) > 1:
+            for prob in matrix_prob_vector:
+                roll = (random.randint(0, 100))/100
+                print(str(roll) + " " + str(prob))
+                if roll > prob:
+                    #that object is now out of the running,
+                    #we won't use it for placement
+                    del_index = matrix_prob_vector.index(prob)
+                    del matrix_prob_vector[del_index]
+                    del possible_indices[del_index]
+                    break 
+        
+        selected_index = possible_indices[0]
+        if selected_index == answer:
+            matrix_correct_counter += 1
+         
+
         total_row_counter += 1
 
 print("Total accuracy of word2vec is:")
@@ -74,4 +130,6 @@ print(str(embedding_correct_counter / total_row_counter))
 print("\nTotal accuracy of wordnet is:")
 print(str(wordnet_correct_counter / total_row_counter))
 
+print("\nTotal accuracy of matrix is:")
+print(str(matrix_correct_counter / total_row_counter))
 
